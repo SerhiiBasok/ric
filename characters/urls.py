64d23f0110717120e64d271bea3f00a154a01827
Serhii Basok <sergeybsk19@gmail.com>
2025-09-26 19:23:45 +0300

    Add `CharacterListView` API endpoint with filtering by name and update URL patterns

--- characters/urls.py
+++ characters/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from characters.views import get_random_character
+from characters.views import get_random_character, CharacterListView
 
 urlpatterns = [
-    path("charakters/random/", get_random_character, ),
+    path("charakters/random/", get_random_character, name="characters-random"),
+    path("charakters/", CharacterListView.as_view(), name="characters" ),
 ]
 
 app_name = "characters"
